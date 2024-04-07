c8a46cae8cd0fd5ac457f15809b7189e3f24153f
jash-jariwala <jashjariwala7911@gmail.com>
2024-04-07 18:40:35 +0530

    init

---
+++ readme.md
@@ -0,0 +1,9 @@
+Generates ascii art for given image
+
+````
+cargo run
+````
+
+## Result
+
+<img src="https://github.com/JASH-JARIWALA/rust-ascii-art/raw/main/result.png" height="500">
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use image::GenericImageView;
+
+fn get_char_from_pix(intent: u8) -> &'static str {
+    let index = intent / 32;
+    let ascii = [" ", ".", ",", "-", "~", "+", "=", "@"];
+    ascii[index as usize]
+}
+
+fn get_image(dir: &str, scale: u32) {
+    let img = image::open(dir).unwrap();
+    println!("{:?}", img.dimensions());
+    let (width, height) = img.dimensions();
+    for y in 0..height {
+        for x in 0..width {
+            if y % (scale * 2) == 0 && x % scale == 0 {
+                let pix = img.get_pixel(x, y);
+
+                let mut intent = pix[0] / 3 + pix[1] / 3 + pix[2] / 3;
+                if pix[3] == 0 {
+                    intent = 0;
+                }
+                print!("{}", get_char_from_pix(intent));
+            }
+        }
+        if y % (scale * 2) == 0 {
+            println!("");
+        }
+    }
+}
+
+fn main() {
+    get_image("./daredevil.png", 3);
+}
